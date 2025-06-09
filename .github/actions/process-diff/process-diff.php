179b88995d35c4c069fc16962263c84fadb814f4
Diomedes Pablo <diomedex10@gmail.com>
2025-06-08 23:59:42 -0500

    mejora

--- .github/actions/process-diff/process-diff.php
+++ .github/actions/process-diff/process-diff.php
@@ -19,7 +19,7 @@
         // 1. Procesar JSON
         $jsonProcessor = new JsonProcessor();
         $array = $jsonProcessor->loadAndCleanJson($diffFile);
-        echo json_encode($array) ." ".$RESET\n";
+        echo json_encode($array) ." $RESET\n";
 
         // 4. Analizar diferencias
         $analyzer = new DiffAnalyzer();
